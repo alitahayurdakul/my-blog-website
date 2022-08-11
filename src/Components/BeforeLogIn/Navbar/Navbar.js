5899bb7324bfef9e98f3efca796c9dbc363064c5
alitahayurdakul <alitahayurdakul@outlook.com>
2022-08-11 16:55:18 +0300

    Navbar is updated

--- src/Components/BeforeLogIn/Navbar/Navbar.js
+++ src/Components/BeforeLogIn/Navbar/Navbar.js
@@ -25,7 +25,7 @@ class Navbar extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-light">
                 <div className="container-fluid">
-                    <Link to="/" className='navbar-brand'><i>Ali 'nin Notları</i></Link >
+                    <Link to="/" className='navbar-brand'><i>Ali'nin Notları</i></Link >
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
