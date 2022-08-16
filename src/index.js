f48854f8b2a9e47b113fc89cf638bb7fdad7c5fe
alitahayurdakul <alitahayurdakul@outlook.com>
2022-08-16 13:24:45 +0300

    HashRouter convert BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import configureStore from './redux/reducers/configureStore';
@@ -9,9 +9,9 @@ import configureStore from './redux/reducers/configureStore';
 const store = configureStore();
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>,
 
   document.getElementById('root')
